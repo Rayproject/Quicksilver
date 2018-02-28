7b39d11f0e04513396811d3fcac6e896a9cd3953
Alex Dragomir <a.dragomir@student.tue.nl>
2018-02-28 20:20:30 +0100

    dummy version

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -6,20 +6,32 @@
 #include "SimpleEstimator.h"
 
 SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
-
+    int length = graph.getNoLabels();
+    uint32_t labelCount[length];
+    uint32_t sol = 0;
     // works only with SimpleGraph
     graph = g;
 }
 
 void SimpleEstimator::prepare() {
 
-    // do your prep here
+    for(int i = 0; i < length; i ++) {
+        labelCount[i] = 0;
+    }
 
+    for(i = 0; i < length; i ++) {
+        for(std::pair p: graph.adj[i]) {
+            labelCount[p.first] ++;
+        }
+    }
+
+    for(i = 0; i < length; i ++) {
+        sol += labelCount[i];
+    }
+    sol /= length;
 }
 
 cardStat SimpleEstimator::estimate(RPQTree *q) {
 
-    // perform your estimation here
-
-    return cardStat {0, 0, 0};
+    return cardStat {sol, sol, sol};
 }
