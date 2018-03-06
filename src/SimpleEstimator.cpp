6cea5874076d9449030f7fe0872a1ff147d69f7c
Alex Dragomir <a.dragomir@student.tue.nl>
2018-03-06 12:06:14 +0100

    Fixed old code, no more errors.

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -5,27 +5,34 @@
 #include "SimpleGraph.h"
 #include "SimpleEstimator.h"
 
+uint32_t length;
+uint32_t labelCount[];
+uint32_t sol;
+
 SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
-    uint32_t length = graph.getNoLabels();
-    uint32_t labelCount[length];
-    uint32_t sol = 0;
+
     // works only with SimpleGraph
     graph = g;
+
+
 }
 
 void SimpleEstimator::prepare() {
+    length = graph.get()->getNoLabels();
+    labelCount = new uint32_t[length];
+    sol = 0;
 
     for(uint32_t i = 0; i < length; i ++) {
         labelCount[i] = 0;
     }
 
-    for(i = 0; i < length; i ++) {
-        for(std::pair p: graph.adj[i]) {
+    for(uint32_t i = 0; i < length; i ++) {
+        for(std::pair p: graph.get()->adj[i]) {
             labelCount[p.first] ++;
         };
     }
 
-    for(i = 0; i < length; i ++) {
+    for(uint32_t i = 0; i < length; i ++) {
         sol = sol + labelCount[i];
     }
     sol = sol / length;
