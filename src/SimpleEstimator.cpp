9812fd7354086743972c51ae4b69b19fa32ec7b0
Alex Dragomir <a.dragomir@student.tue.nl>
2018-03-08 19:49:34 +0100

    Fixed a bug (cardStat should be {out, paths, in}, not {in, out, paths}.

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -112,7 +112,7 @@ cardStat SimpleEstimator::estimate(RPQTree *q) {
             uint32_t in = left.noIn;
             uint32_t out = right.noOut;
             auto paths = std::min(left.noPaths * right.noPaths /(right.noIn/4),left.noPaths * right.noPaths /(left.noOut/4));
-            cardStat processed = cardStat{in,out,paths};
+            cardStat processed = cardStat{out, paths, in};
             left = processed;
         }
         queryVector.clear();
