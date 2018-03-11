8dbd9087d3206dc32aba5634eb9e5bb9f11896f6
Rayproject <36917864+Rayproject@users.noreply.github.com>
2018-03-11 03:49:03 +0100

    Update SimpleEstimator.cpp

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -114,8 +114,8 @@ cardStat SimpleEstimator::estimate(RPQTree *q) {
             uint32_t in = left.noIn / 2;
             uint32_t out = right.noOut / 2;
 
-            auto paths = std::min(2 * left.noPaths * right.noPaths / (right.noIn),
-                                  2 * left.noPaths * right.noPaths / (left.noOut));
+            auto paths = std::min(2 * left.noPaths * right.noPaths / (right.noIn+1),
+                                  2 * left.noPaths * right.noPaths / (left.noOut+1));
             cardStat processed = cardStat{std::min(out, paths), paths, std::min(in, paths)};
             left = processed;
         }
