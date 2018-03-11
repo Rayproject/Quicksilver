6ee07153b98f18280669120a24fc3bcc58adc64b
Alex Dragomir <a.dragomir@student.tue.nl>
2018-03-11 19:12:13 +0100

    asd

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -114,8 +114,8 @@ cardStat SimpleEstimator::estimate(RPQTree *q) {
             uint32_t in = (left.noIn)/ 4;
             uint32_t out = (right.noOut) / 4;
         
-            auto paths = std::min(2 * left.noPaths * right.noPaths / (right.noIn + right.noOut + 1),
-                                  2 * left.noPaths * right.noPaths / (left.noOut + left.noIn + 1));
+            auto paths = std::min(2 * left.noPaths * right.noPaths / (right.noIn + right.noOut + 5),
+                                  2 * left.noPaths * right.noPaths / (left.noOut + left.noIn + 5));
             cardStat processed = cardStat{std::min(out, paths), paths, std::min(in, paths)};
             left = processed;
         }
